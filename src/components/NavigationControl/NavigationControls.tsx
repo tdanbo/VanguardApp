e4702e694156bdfd966cf0399a834c4691f5bf3b
tdanbo <46909851+tdanbo@users.noreply.github.com>
2023-09-14 19:50:06 +0200

    .

---
+++ src/components/NavigationControl/NavigationControls.tsx
@@ -0,0 +1,14 @@
+import styled from "styled-components";
+
+const Container = styled.div`
+  display: flex;
+  flex: 1;
+  flex-direction: column;
+  background-color: coral;
+`;
+
+function NavigationControls() {
+  return <Container>abc</Container>;
+}
+
+export default NavigationControls;
