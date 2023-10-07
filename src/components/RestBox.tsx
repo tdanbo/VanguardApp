1b95466a2091a17408dfdbd148ec54af27a2958b
tdanbo <46909851+tdanbo@users.noreply.github.com>
2023-10-07 23:32:50 +0200

    removed line

--- src/components/RestBox.tsx
+++ src/components/RestBox.tsx
@@ -39,7 +39,6 @@ function RestBox() {
     console.log("Resting");
     console.log("Character: ", character);
     console.log("Expanding");
-    console.log("Character: ", updatedCharacter);
 
     // Using the handleRoll function here doesn't make much sense unless you are planning to call this function somewhere else.
     // Otherwise, you can directly call onRollDice with the required parameters.
