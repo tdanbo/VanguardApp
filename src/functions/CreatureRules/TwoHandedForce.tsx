2e4b754695c3b2641aabffa1dfdf6e13625a2e04
tdanbo <46909851+tdanbo@users.noreply.github.com>
2023-11-08 23:45:58 +0100

    adding more creature rules

--- src/functions/CreatureRules/TwoHandedForce.tsx
+++ src/functions/CreatureRules/TwoHandedForce.tsx
@@ -1,50 +1,44 @@
 import { modifiedCreature } from "../../Types";
 import { cloneDeep } from "lodash";
-export const TwinAttack = (
+export const TwoHandedForce = (
   modifiedCreature: modifiedCreature,
   creatureAbilities: Record<string, number>,
 ) => {
   const clonedCreature = cloneDeep(modifiedCreature);
 
-  if (creatureAbilities["Twin Attack"] === 1) {
+  if (creatureAbilities["Two-handed Force"] === 1) {
     for (const weapon of clonedCreature.weapon) {
       if (
         weapon.type !== "Ranged Weapon" &&
         weapon.type !== "Throwing Weapon"
       ) {
-        weapon.roll.dice = 8;
+        weapon.roll.dice = 12;
       }
     }
-
-    clonedCreature.attacks += 1;
-    clonedCreature.attacks_mod = -1;
-    clonedCreature.defense -= 1;
-  } else if (creatureAbilities["Twin Attack"] === 2) {
+  } else if (creatureAbilities["Two-handed Force"] === 2) {
     for (const weapon of clonedCreature.weapon) {
       if (
         weapon.type !== "Ranged Weapon" &&
         weapon.type !== "Throwing Weapon"
       ) {
-        weapon.roll.dice = 8;
+        weapon.roll.dice = 12;
       }
     }
 
     clonedCreature.attacks += 1;
-    clonedCreature.attacks_mod = 0;
-    clonedCreature.defense -= 1;
-  } else if (creatureAbilities["Twin Attack"] === 3) {
+    clonedCreature.attacks_mod = -2;
+  } else if (creatureAbilities["Two-handed Force"] === 3) {
     for (const weapon of clonedCreature.weapon) {
       if (
         weapon.type !== "Ranged Weapon" &&
         weapon.type !== "Throwing Weapon"
       ) {
-        weapon.roll.dice = 10;
+        weapon.roll.dice = 12;
       }
     }
 
     clonedCreature.attacks += 1;
-    clonedCreature.attacks_mod = -1;
-    clonedCreature.defense -= 1;
+    clonedCreature.attacks_mod = -2;
   }
 
   return clonedCreature;
