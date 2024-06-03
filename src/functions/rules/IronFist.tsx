38e7a52cf58545846cc28c010500fec994f5733f
tdanbo <46909851+tdanbo@users.noreply.github.com>
2024-06-03 19:11:15 +0200

    .

--- src/functions/rules/IronFist.tsx
+++ src/functions/rules/IronFist.tsx
@@ -5,16 +5,13 @@ export function IronFist_dice(character: CharacterEntry, item: ItemEntry) {
   const ability_name = "Iron Fist";
   const ability = CheckAbility(character, ability_name, "novice");
   const ability_adept = CheckAbility(character, ability_name, "adept");
-  const ability_master = CheckAbility(character, ability_name, "master");
 
   let mod = 0;
   if (!IsMeleeWeapon(item)) {
     return mod;
   }
 
-  if (ability_master) {
-    mod += 8;
-  } else if (ability_adept) {
+  if (ability_adept) {
     mod += 4;
   } else if (ability) {
     return mod;
